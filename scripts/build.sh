30df1fd030d59e076c4f6e0612d665392611e4a6
Denis Belyakov <sanityseeker@ya.ru>
2022-06-06 03:06:14 +0100

    update sources

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,7 @@ SITE_DIR="docs/"
 PROJECT_DIR="/home/pi/belfilm"
 
 cd $PROJECT_DIR; cd assets && listdirs img/ | shuf | head -n 50 > randomImages; cd $PROJECT_DIR;
-hugo -D
+hugo --gc -D
 echo "Enter root password to copy from $SITE_DIR to $WEBSERVER_DIR"
 read -s pass
 echo pass | sudo -S rsync -auhr --info=progress2 $SITE_DIR $WEBSERVER_DIR --delete
